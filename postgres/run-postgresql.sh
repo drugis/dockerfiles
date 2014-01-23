ce0c0a7bd89cac3ab81b6cd812133a109a8e5edb
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2014-01-23 10:47:54 +0100

    postgresql container

---
+++ postgres/run-postgresql.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+su postgres sh -c "/usr/lib/postgresql/9.1/bin/postgres --single -D /var/lib/postgresql/9.1/main -c config_file=/etc/postgresql/9.1/main/postgresql.conf" <<< "ALTER USER postgres WITH PASSWORD '$1';"
+su postgres -c "/usr/lib/postgresql/9.1/bin/postgres -D /var/lib/postgresql/9.1/main -c config_file=/etc/postgresql/9.1/main/postgresql.conf"
