1e82dce930ee6cc6d19f914c5fac4ded8a9e2a3d
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2014-02-03 16:43:44 +0100

    Move mailman data to volume

--- lists.drugis.org/start-all.sh
+++ lists.drugis.org/start-all.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# make sure mailman can access the data
+chown -R root.list /srv/mailman/data /srv/mailman/lists /srv/mailman/private
+check_perms -f
+
 service exim4 start
 service mailman start
 
