c84947e1c9964b40069a5b3529dc330a8123c586
Daan Reid <osmosisch@gmail.com>
2014-04-03 09:21:10 +0200

    Change from APT tomcat to manual download/install

--- tomcat/tomcat7-run.sh
+++ tomcat/tomcat7-run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # remove old unpack
-rm -rf /var/lib/tomcat7/webapps/ROOT
-
-# the tomcat configuration home
-export CATALINA_BASE=/var/lib/tomcat7
+rm -rf /usr/share/tomcat7/webapps/ROOT
 
 # run tomcat without daemonizing
 /usr/share/tomcat7/bin/catalina.sh run
